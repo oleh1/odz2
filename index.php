86eb4ede292ff885c6b13fbc29b794da22ffe2e2
Oleh1 <lyboleg@gmail.com>
2016-04-05 20:49:10 +0300

    Koment2

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
-<html>
-<head><title></title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <script src="js.js"></script>
-</head>
-<body>
 <?php
 session_start();
 define('BASE_PATH', realpath(dirname(__FILE__)));
 require 'Loader.php';
 $loader = new Loader();
 $router = new router();
+$rout = $router->start();
 ?>
+<html>
+<head><title></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="js.js"></script>
+</head>
+<body>
 <div class="home1" align="center">
 <h1 align="center">Интернет магазин электронных калькуляторов</h1><br>
 <b>Добро пожалувать в магазин Копицы О. В. Группа Ит.мз-52с</b><br>
 </div>
 <div class="home2">
-    <?php echo $router->start(); ?>
+    <?= $rout; ?>
 </div>
 <hr>
 <hr>
